27b7a88935e879780887e07eb4ba3dec7166d7df
MatveyVi <fanterwatch@gmail.com>
2025-10-07 19:21:49 +0300

    main ts for auth

---
+++ apps/auth/src/main.ts
@@ -0,0 +1,20 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 4001,
+      },
+    }
+  );
+  await app.listen();
+  Logger.log(`🚀 Auth Service is listening on TCP port 4001`);
+}
+
+bootstrap();
