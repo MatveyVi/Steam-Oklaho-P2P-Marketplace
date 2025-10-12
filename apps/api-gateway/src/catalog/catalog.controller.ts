3aa9a1fee06fa7f3a3886e6b70d7de31cb3c1cc1
MatveyVi <fanterwatch@gmail.com>
2025-10-12 14:24:36 +0300

    Added: Auth guard to catalog endpoints

--- apps/api-gateway/src/catalog/catalog.controller.ts
+++ apps/api-gateway/src/catalog/catalog.controller.ts
@@ -1,17 +1,20 @@
 import { HttpService } from '@nestjs/axios';
 import { Controller, Get, Param } from '@nestjs/common';
 import { lastValueFrom } from 'rxjs';
+import { Auth } from '../app/decorators/auth.decorator';
 
 @Controller('catalog')
 export class CatalogController {
   constructor(private readonly httpService: HttpService) {}
 
+  @Auth()
   @Get('/items')
   async findAll() {
     const response = await lastValueFrom(this.httpService.get('/items'));
     return response.data;
   }
 
+  @Auth()
   @Get('/items/:externalId')
   async findOne(@Param('externalId') externalId: string) {
     const response = await lastValueFrom(
